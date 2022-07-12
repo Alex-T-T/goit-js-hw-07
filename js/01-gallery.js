ae39f3cdbab9516ccb7806914955246bf3746336
Olexandr Tolochko <0504fusion@gmail.com>
2022-07-12 23:58:52 +0300

    Summary done p.1,3 task 1
    add files of lightBox
    done p.1,3 task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+// ========== 1. Створення і рендер розмітки ==========
+const gallery = document.querySelector('.gallery');
+
+const galleryRef = (items) => 
+    items.map(item => `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`).join(' ');    
+
+const addGalleryItems = galleryRef(galleryItems);
+console.log(addGalleryItems);
+
+gallery.insertAdjacentHTML('afterbegin', addGalleryItems);
+
+// ==============================
+// ==============================
+
+// ========== 2. Реалізація делегування на div.gallery ==========
+
